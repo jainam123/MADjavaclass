ae5752dde258b55dbc1e19a33ba927698f7554b9
MacStudent <macstudent@20X-iMacXX-M-441.local>
2018-06-01 14:43:36 -0400

    first commit

---
+++ src/employee.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author macstudent
+ */
+public class employee {
+    int employeeid;
+
+    public int getEmployeeid() {
+        return employeeid;
+    }
+
+    public String getEmployeename() {
+        return employeename;
+    }
+    String employeename;
+    
+}
